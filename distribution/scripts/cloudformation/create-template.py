88b7e8ee0eeee33a9c3b32876b2ee58f9a097873
Janeth <janethavishka@gmail.com>
2019-09-05 14:15:13 +0530

    Get argument types

--- distribution/scripts/cloudformation/create-template.py
+++ distribution/scripts/cloudformation/create-template.py
@@ -65,12 +65,12 @@ def main():
                         action="store_true", help="Start bastion instance")
     parser.add_argument("--enable-cloudwatch-logs", default=False,
                         action="store_true", help="Enable AWS CloudWatch logs")
+    args = parser.parse_args()
     type(args.region)
     type(args.ami_id)
     type(args.jmeter_servers)
     type(args.start_bastion)
     type(args.output_name)
-    args = parser.parse_args()
 
     context = {
         'region': args.region,
